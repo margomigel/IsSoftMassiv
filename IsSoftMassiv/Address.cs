4eea2b2efd3165b9e6bd6448cda48f3fe02ee5ce
Margo Migel <Margomigel@gmail.com>
2022-12-30 22:15:26 +0100

    homework-holidays

---
+++ IsSoftMassiv/Address.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsSoftMassiv
+{
+    public class Address
+    {
+        public string City;
+        public string Street;
+        public int House;
+        public int Appartment;
+
+        public Address (string city, string street, int house, int appartment)
+        {
+            City = city;
+            Street = street;
+            House = house;
+            Appartment = appartment;
+        }
+    }
+}
