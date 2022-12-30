9cfaa5e88435cdf7c80176183d28be87be83d22e
Margo Migel <Margomigel@gmail.com>
2022-12-30 22:15:26 +0100

    homework-holidays

---
+++ IsSoftMassiv/Candidate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsSoftMassiv
+{
+    public class Candidate
+    {
+        public Person Person;
+        public SubjectScore[] SubjectScore;
+        
+        public Candidate(Person person, SubjectScore[] subjectScore)
+        {
+            Person = person;
+            SubjectScore = subjectScore; 
+        }
+    }
+}
