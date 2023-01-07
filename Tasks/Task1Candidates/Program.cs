b06daa4ca7245d1bb898a183e119b720b790c1b7
Margo Migel <Margomigel@gmail.com>
2023-01-07 20:53:28 +0100

    Refactoring

---
+++ Tasks/Task1Candidates/Program.cs
@@ -0,0 +1,112 @@
+﻿using Task1Candidates;
+using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+{
+    const string SUBJECT = "hist";
+    private static void Main(string[] args)
+    {
+
+        Person personIvanov = new Person("Ivan", "Ivanov",
+            new Address(
+                "Minsk",
+                "Kalinovskogo",
+                23,
+                50
+                )
+            );
+        SubjectScore ivanovMath = new SubjectScore("math", 10);
+        SubjectScore ivanovChem = new SubjectScore("chem", 9);
+        SubjectScore ivanovHist = new SubjectScore("hist", 9);
+        SubjectScore ivanovPhis = new SubjectScore("phis", 8);
+        SubjectScore ivanovLang = new SubjectScore("lang", 10);
+
+        SubjectScore[] ivanovSubjectScore = {
+            ivanovMath,
+            ivanovChem,
+            ivanovHist,
+            ivanovPhis,
+            ivanovLang
+        };
+
+        Candidate ivanov = new Candidate(personIvanov, ivanovSubjectScore);
+
+        Candidate petrov = new Candidate(
+            new Person("Petr", "Petrov", new Address("Minsk", "Chornogo", 12, 31)),
+            new SubjectScore[]{
+                new SubjectScore("math", 9),
+                new SubjectScore("chem", 10),
+                new SubjectScore("hist", 6),
+                new SubjectScore("phis", 7),
+                new SubjectScore("lang", 8)
+            }
+        );
+
+        Candidate sidorov = new Candidate(
+            new Person("Alexandr", "Sidorov", new Address("Minsk", "Kolasa", 1, 20)),
+            new SubjectScore[]{
+                new SubjectScore("math", 6),
+                new SubjectScore("chem", 9),
+                new SubjectScore("hist", 9),
+                new SubjectScore("phis", 8),
+                new SubjectScore("lang", 10)
+            }
+        );
+
+        Candidate kulikova = new Candidate(
+            new Person("Anna", "Kulikova", new Address("Grodno", "Gorkogo", 31, 83)),
+            new SubjectScore[]{
+                new SubjectScore("math", 10),
+                new SubjectScore("chem", 10),
+                new SubjectScore("hist", 8),
+                new SubjectScore("phis", 9),
+                new SubjectScore("lang", 9)
+            }
+        );
+
+        Candidate mikulina = new Candidate(
+            new Person("Maria", "Mikulina", new Address("Grodno", "BLK", 24, 1)),
+            new SubjectScore[]{
+                new SubjectScore("math", 10),
+                new SubjectScore("chem", 8),
+                new SubjectScore("hist", 9),
+                new SubjectScore("phis", 10),
+                new SubjectScore("lang", 9)
+            }
+        );
+
+        Candidate[] candidates = {
+            ivanov,
+            petrov,
+            sidorov,
+            kulikova,
+            mikulina
+        };
+        ShowInfoStreets(candidates);
+        ShowMaxGradeSubject(candidates, SUBJECT)  ;
+    }
+
+    static void ShowInfoStreets(Candidate[] candidates)
+    {
+        Console.WriteLine("Candidate's streets are: ");
+        foreach ( Candidate candidate in candidates)
+        {
+            Console.WriteLine(candidate.Person.Address.Street + " ");
+        }
+        Console.WriteLine();
+    }
+
+    static void ShowMaxGradeSubject(Candidate[] candidates, string subject) 
+        {
+            int maxGrade = 0;
+            foreach (Candidate candidate in candidates)
+            {
+                foreach (SubjectScore subjectScore in candidate.SubjectScore) 
+                {
+                    if (subjectScore.Subject == subject && subjectScore.Score > maxGrade) 
+                        maxGrade = subjectScore.Score;
+                }
+            }
+            Console.WriteLine($"Max {subject}'s grade is {maxGrade}");
+        }
+}
---
+++ Tasks/Task1Candidates/SubjectScore.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1Candidates
+{
+    public class SubjectScore
+    {
+        const int MAX_CORE = 10;
+        private int _score;
+        public string Subject { get; set; }
+        public int Score { 
+            get
+            {
+                return _score;
+            }
+            
+            set
+            { 
+                if (value > 0 && value <= MAX_CORE)
+                {
+                    _score = value; 
+                }
+            }
+        }
+
+        public SubjectScore(string subject, int score)
+        {
+            Subject = subject;
+            _score = score;
+        }
+
+    }
+
+}
